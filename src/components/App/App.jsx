d480b22add9e327abcf3390ff21d297af41bf9d7
Andreas Copan <avcopan@gmail.com>
2023-07-03 13:59:34 -0400

    Completes #2
    Displays pizzas to the DOM, along with their prices and descriptions.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,52 +4,42 @@ import Header from '../Header/Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPizzas } from '../../modules/pizza.request';
 import './App.css';
-import { ListGroup, ListGroupItem } from "reactstrap";
-
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 function App() {
   const dispatch = useDispatch();
-  const pizzaList = useSelector((store) => {
-    store.pizza
-  });
+  const pizzaList = useSelector((store) => store.pizza);
 
   useEffect(() => {
-    getPizzas().then((pizza) => {
-      dispatch({ type: "SET_PIZZA_LIST", payload: pizza });
+    getPizzas().then((pizzas) => {
+      dispatch({ type: 'SET_PIZZA_LIST', payload: pizzas });
     });
   }, []);
 
   return (
     <>
-    <div className="App">
-      <Header />
-      <img src="images/pizza_photo.png" />
-      <p>Pizza is great.</p>
-    </div>
+      <div className="App">
+        <Header />
+        <img src="images/pizza_photo.png" />
+        <p>Pizza is great.</p>
+      </div>
 
-    <div className="Table"> 
-      <ListGroup flush>
-        {}
-        <ListGroupItem disabled href="#" tag="a">
-          Pizza
-        </ListGroupItem>
-        <ListGroupItem href="#" tag="a">
-          Pizza2
-        </ListGroupItem>
-        <ListGroupItem href="#" tag="a">
-          Pizza3
-        </ListGroupItem>
-        <ListGroupItem href="#" tag="a">
-          Pizza4
-        </ListGroupItem>
-        <ListGroupItem href="#" tag="a">
-          Pizza5
-        </ListGroupItem>
-      </ListGroup>
-    </div>
-  </>
+      <div className="Table">
+        <ListGroup flush>
+          {}
+          <ListGroupItem disabled href="#" tag="a">
+            Pizza
+          </ListGroupItem>
+          {pizzaList.map((pizza) => (
+            <ListGroupItem href="#" tag="a" key={pizza.id}>
+              <ListGroupItemHeading>{pizza.name} ${pizza.price}</ListGroupItemHeading>
+              <ListGroupItemText>{pizza.description}</ListGroupItemText> 
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+    </>
   );
-  
 }
 
 export default App;
